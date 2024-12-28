5ff2fafad9c8e74ad882e9fd4638b550c8fc65ad
Du <dunguyen241003@gmail.com>
2024-12-28 07:28:03 +0000

    Thông điệp commit mô tả thay đổi

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
+  before_action :set_product, only: %i[ show edit update destroy ]
+
   def index
     @products = Product.all
   end
@@ -20,8 +23,28 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description, :featured_image ])
     end
 end
